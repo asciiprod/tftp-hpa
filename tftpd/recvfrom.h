3f7deeecdcf1e9eadbe50d04e2022a984edba458
Markus Schade <markus.schade@gmail.com>
2016-06-07 20:29:25 +0200

    Imported Upstream version 5.2+20150808

---
+++ tftpd/recvfrom.h
@@ -0,0 +1,22 @@
+/* ----------------------------------------------------------------------- *
+ *
+ *   Copyright 2001-2006 H. Peter Anvin - All Rights Reserved
+ *
+ *   This program is free software available under the same license
+ *   as the "OpenBSD" operating system, distributed at
+ *   http://www.openbsd.org/.
+ *
+ * ----------------------------------------------------------------------- */
+
+/*
+ * recvfrom.h
+ *
+ * Header for recvfrom substitute
+ *
+ */
+
+#include "config.h"
+
+int
+myrecvfrom(int s, void *buf, int len, unsigned int flags,
+           union sock_addr *from, union sock_addr *myaddr);
